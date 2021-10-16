0bb7b287f97c8a53d1ff7dc7464467b8588d994d
Malte Kiefer <malte@radermacher-consulting.de>
2021-10-16 14:57:28 +0200

    add api url for firewall

--- api/server/urls.py
+++ api/server/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name='server'
 urlpatterns=[
     path('common/', views.StatsView.as_view(), name='stats'),
-    path('hardware/', views.HardwareinfoView.as_view(), name='hardwareinfo')
+    path('hardware/', views.HardwareinfoView.as_view(), name='hardwareinfo'),
+    path('firewall/', views.FirewallView.as_view(), name='firewall')
 ]
